7c470fc50972a0450bb01dc21143374e7a0246a7
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-26 15:36:05 +0500

    Update App.js

--- my-sarcon/App.js
+++ my-sarcon/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./screens/Splash";
+import Login from "./screens/Login";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }} // Global setting for all screens
+      >
         <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
