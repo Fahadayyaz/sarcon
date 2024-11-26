d321061af320b4e70d206c3be7cabd404614ab23
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-26 15:36:21 +0500

    Update splash.jsx

--- my-sarcon/screens/splash.jsx
+++ my-sarcon/screens/splash.jsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
-const splash = () => {
+const Splash = ({ navigation }) => {
+  useEffect(() => {
+    // 3 seconds ke baad Login screen par navigate karega
+    const timer = setTimeout(() => {
+      navigation.navigate("Login");
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup timer
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>splash</Text>
-    </View>
-  )
-}
+    <ImageBackground
+      source={require("./../assets/splashScreen.png")}
+      style={styles.container}
+      resizeMode="cover"
+    >
+      <Text style={styles.text}>Welcome</Text>
+    </ImageBackground>
+  );
+};
 
-export default splash
+export default Splash;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
