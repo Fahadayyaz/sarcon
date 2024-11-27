8a89a753ef0a0345d7a278dde952d2e099eb9457
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-27 14:44:42 +0500

    Create ProviderSignIn.jsx

---
+++ screens/provider/ProviderSignIn.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderSignIn = () => {
+  return (
+    <View>
+      <Text>Provider SignIn</Text>
+    </View>
+  );
+};
+
+export default ProviderSignIn;
+
+const styles = StyleSheet.create({});
