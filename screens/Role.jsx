65f2934a987cd74c5fa5ecb28ff75ec00783c328
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-27 09:35:09 +0500

    Create Role.jsx

---
+++ screens/Role.jsx
@@ -0,0 +1,45 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  Image,
+} from "react-native";
+import React from "react";
+
+const Login = () => {
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <StatusBar barStyle="auto" backgroundColor="#ffffff" />
+      <View style={styles.contentContainer}>
+        <Image
+          source={require("./../assets/sarconIcon.png")}
+          style={{
+            width: 184,
+            height: 62,
+            alignSelf: "center",
+            marginBottom: 20,
+          }}
+        />
+        <Text style={{ fontWeight: "bold", fontSize: 28 }}>Are You!</Text>
+        <Text style={{ color: "gray", fontSize: 14 }}>
+          Please Select your role for this app.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+  },
+  contentContainer: {
+    width: "90%",
+  },
+});
