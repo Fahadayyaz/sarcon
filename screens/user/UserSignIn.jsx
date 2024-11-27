ba011946f42416ec4e33688cb2e6b742786a9a2a
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-27 15:32:00 +0500

    Update UserSignIn.jsx

--- screens/user/UserSignIn.jsx
+++ screens/user/UserSignIn.jsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
 
 const UserSignIn = () => {
+  const { width, height } = Dimensions.get("window");
+
   return (
-    <View>
-      <Text>User SignIn</Text>
+    <View style={[styles.container, { width, height }]}>
+      <Text style={styles.text}>UserSignIn</Text>
     </View>
   );
 };
 
 export default UserSignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 30,
+  },
+});
