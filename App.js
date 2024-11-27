0f11782bfd2d3da362b6a00d72f246581cec772b
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-27 10:30:36 +0500

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,19 @@ import Role from "./screens/Role";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Poppins_SemiBold: require("../Zyra/assets/fonts/Poppins-SemiBold.ttf"),
+    Poppins_Medium: require("../Zyra/assets/fonts/Poppins-Medium.ttf"),
+    PlusJakartaSans_Regular: require("../Zyra/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    PlusJakartaSans_Medium: require("../Zyra/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    PlusJakartaSans_Bold: require("../Zyra/assets/fonts/PlusJakartaSans-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
