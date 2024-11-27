dc80a0da9a85e5bb2e6947eb91d6c3857140f437
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-27 15:58:05 +0500

    Update ProviderSignIn.jsx

--- screens/provider/ProviderSignIn.jsx
+++ screens/provider/ProviderSignIn.jsx
@@ -1,14 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const ProviderSignIn = () => {
   return (
-    <View>
-      <Text>Provider SignIn</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style={"auto"} />
+      <Text style={styles.text}>Provider SignIn</Text>
+    </SafeAreaView>
   );
 };
 
 export default ProviderSignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  text: {
+    fontSize: 30,
+  },
+});
