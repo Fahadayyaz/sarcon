010eeae3543aea497329c3fa1c9ae74f741c7bc5
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-28 17:07:04 +0500

    Create ProviderForgotPassword.jsx

---
+++ screens/provider/ProviderForgotPassword.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderForgotPassword = () => {
+  return (
+    <View>
+      <Text>ProviderForgotPassword</Text>
+    </View>
+  );
+};
+
+export default ProviderForgotPassword;
+
+const styles = StyleSheet.create({});
