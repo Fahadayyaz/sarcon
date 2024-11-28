fd173d7dff96d6edb803eebb0b6740b7f438a442
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-28 13:59:54 +0500

    Create ProviderSignUp.jsx

---
+++ screens/provider/ProviderSignUp.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderSignUp = () => {
+  return (
+    <View>
+      <Text>ProviderSignUp</Text>
+    </View>
+  );
+};
+
+export default ProviderSignUp;
+
+const styles = StyleSheet.create({});
