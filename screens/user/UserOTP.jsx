74f323e7e9bbefad6f6571516fbca2368b90024a
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-30 14:21:21 +0500

    Create UserOTP.jsx

---
+++ screens/user/UserOTP.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserOTP = () => {
+  return (
+    <View>
+      <Text>UserOTP</Text>
+    </View>
+  );
+};
+
+export default UserOTP;
+
+const styles = StyleSheet.create({});
