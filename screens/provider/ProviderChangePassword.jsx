6c2cf2244edf99028b31f6d3bf07d6bb68443ec9
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-30 17:34:31 +0500

    Create ProviderChangePassword.jsx

---
+++ screens/provider/ProviderChangePassword.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderChangePassword = () => {
+  return (
+    <View>
+      <Text>ChangePassword</Text>
+    </View>
+  );
+};
+
+export default ProviderChangePassword;
+
+const styles = StyleSheet.create({});
