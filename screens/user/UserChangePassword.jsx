c511a02320d4c22d5f783fdcc257c86f19937647
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-30 17:34:34 +0500

    Create UserChangePassword.jsx

---
+++ screens/user/UserChangePassword.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserChangePassword = () => {
+  return (
+    <View>
+      <Text>UserChangePassword</Text>
+    </View>
+  )
+}
+
+export default UserChangePassword
+
+const styles = StyleSheet.create({})
