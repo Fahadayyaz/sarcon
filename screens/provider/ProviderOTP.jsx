3c575e05d2d7f354c7fcbe56e8d3d81a1425961f
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-30 14:21:15 +0500

    Create ProviderOTP.jsx

---
+++ screens/provider/ProviderOTP.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderOTP = () => {
+  return (
+    <View>
+      <Text>ProviderOTP</Text>
+    </View>
+  );
+};
+
+export default ProviderOTP;
+
+const styles = StyleSheet.create({});
