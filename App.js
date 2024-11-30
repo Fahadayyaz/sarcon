941362122047e565328d6fde063c391714b4779b
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-11-30 14:21:04 +0500

    Update App.js

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="UserForgotPassword"
+        initialRouteName="Splash"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Splash" component={Splash} />
