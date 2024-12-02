481a89050941d0be3dfc27ef12d052e34b70e961
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 13:47:20 +0500

    Create UserEmailVerification.jsx

---
+++ screens/user/UserEmailVerification.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserEmailVerification = () => {
+  return (
+    <View>
+      <Text>UserEmailVerification</Text>
+    </View>
+  );
+};
+
+export default UserEmailVerification;
+
+const styles = StyleSheet.create({});
