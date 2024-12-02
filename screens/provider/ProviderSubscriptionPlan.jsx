77d73045bc6504842d22e8282f4c99018cf1ccb8
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 20:11:48 +0500

    Create ProviderSubscriptionPlan.jsx

---
+++ screens/provider/ProviderSubscriptionPlan.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderSubscriptionPlan = () => {
+  return (
+    <View>
+      <Text>ProviderSubscriptionPlan</Text>
+    </View>
+  );
+};
+
+export default ProviderSubscriptionPlan;
+
+const styles = StyleSheet.create({});
