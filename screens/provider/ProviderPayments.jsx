723745df99c25f8f53ac152badc50a6e4a2540da
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 20:11:40 +0500

    Create ProviderPayments.jsx

---
+++ screens/provider/ProviderPayments.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderPayments = () => {
+  return (
+    <View>
+      <Text>ProviderPayments</Text>
+    </View>
+  );
+};
+
+export default ProviderPayments;
+
+const styles = StyleSheet.create({});
