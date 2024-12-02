a8161e7faae575370ef1dda655795bd9dac42028
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 20:11:37 +0500

    Create ProviderCardDetail.jsx

---
+++ screens/provider/ProviderCardDetail.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProviderCardDetail = () => {
+  return (
+    <View>
+      <Text>ProviderCardDetail</Text>
+    </View>
+  );
+};
+
+export default ProviderCardDetail;
+
+const styles = StyleSheet.create({});
