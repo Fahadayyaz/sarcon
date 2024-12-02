79a12e7ee8469f581e2f4ed9064db76ec48ff92d
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 20:11:55 +0500

    Create UserCardDetail.jsx

---
+++ screens/user/UserCardDetail.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserCardDetail = () => {
+  return (
+    <View>
+      <Text>UserCardDetail</Text>
+    </View>
+  );
+};
+
+export default UserCardDetail;
+
+const styles = StyleSheet.create({});
