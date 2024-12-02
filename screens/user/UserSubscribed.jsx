6feb8b602b4c5052defd0ecc70227d7a6d5949e2
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-02 20:12:07 +0500

    Create UserSubscribed.jsx

---
+++ screens/user/UserSubscribed.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UserSubscribed = () => {
+  return (
+    <View>
+      <Text>UserSubscribed</Text>
+    </View>
+  );
+};
+
+export default UserSubscribed;
+
+const styles = StyleSheet.create({});
