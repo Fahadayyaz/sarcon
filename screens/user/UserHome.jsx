fa777691b54112a58c7e0bca906758397f1c9117
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-05 16:11:53 +0500

    Update UserHome.jsx

--- screens/user/UserHome.jsx
+++ screens/user/UserHome.jsx
@@ -1,13 +1,65 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import { useNavigation } from "@react-navigation/native";
 
 const UserHome = () => {
+  const [isEmailFocused, setEmailFocused] = useState(false);
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.content__Container}></View>
+      <View style={styles.content__Container}>
+        <View style={styles.header}>
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.location__logo}>
+              <FontAwesome6 name="location-dot" size={24} color="#0474ED" />
+            </View>
+            <View style={styles.header__text__box}>
+              <Text>My Location</Text>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={{ color: "#0474ED", fontWeight: "bold" }}>
+                  Barcelona, Spain
+                </Text>
+                <AntDesign
+                  name="down"
+                  size={14}
+                  color="black"
+                  style={{ marginTop: 3, marginLeft: 5 }}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.header__display__picture}></View>
+        </View>
+        {/* email input container */}
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            borderColor: isEmailFocused ? "#0474ED" : "#C9C9C9",
+            borderRadius: 44,
+            paddingLeft: 10,
+            marginTop: 15,
+            backgroundColor: "#F2F2F2",
+          }}
+        >
+          <Fontisto name="search" size={24} color="#C9C9C9" />
+          <TextInput
+            placeholder="Search here"
+            placeholderTextColor={isEmailFocused ? "#0474ED" : "#838383"}
+            style={{ width: "100%", height: 48 }}
+            onFocus={() => setEmailFocused(true)}
+            onBlur={() => setEmailFocused(false)}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -23,6 +75,27 @@ const styles = StyleSheet.create({
     width: "90%",
     height: "100%",
     alignSelf: "center",
-    backgroundColor: "blue",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  location__logo: {
+    width: 55,
+    height: 53,
+    borderRadius: 9,
+    backgroundColor: "#F2F8FE",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header__text__box: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  header__display__picture: {
+    width: 49,
+    height: 49,
+    borderRadius: 50,
+    backgroundColor: "orange",
   },
 });
