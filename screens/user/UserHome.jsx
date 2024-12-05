f136052c18b949a6170b14de410b0d1d50d6b25e
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-05 11:14:31 +0500

    Update UserHome.jsx

--- screens/user/UserHome.jsx
+++ screens/user/UserHome.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const UserHome = () => {
   return (
-    <View>
-      <Text>UserHome</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.content__Container}></View>
+    </SafeAreaView>
+  );
+};
 
-export default UserHome
+export default UserHome;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content__Container: {
+    width: "90%",
+    height: "100%",
+    alignSelf: "center",
+    backgroundColor: "blue",
+  },
+});
