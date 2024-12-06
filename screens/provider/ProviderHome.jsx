61d0ba425ada40fc795beedd6cb52a66599f271c
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-06 23:33:56 +0500

    Update ProviderHome.jsx

--- screens/provider/ProviderHome.jsx
+++ screens/provider/ProviderHome.jsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const ProviderHome = () => {
   return (
-    <View>
-      <Text>ProviderHome</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header__container}>
+        <Text style={{ color: "#fff" }}>Header goes here</Text>
+      </View>
+      <View style={styles.content__container}></View>
+    </SafeAreaView>
   );
 };
 
 export default ProviderHome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content__container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+  },
+});
