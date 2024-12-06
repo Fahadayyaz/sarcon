62979a14dd3e3cdb358015ee42ef64907c462d75
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-06 20:41:42 +0500

    Update Test.jsx

--- screens/Test.jsx
+++ screens/Test.jsx
@@ -1,58 +1,15 @@
-import { View, Text, Image, Button, Modal, StyleSheet } from "react-native";
-import React, { useState, useEffect } from "react";
-import CustomPopup from "../components/CustomPopup";
-import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import CustomTabBar from '../components/CustomTabBar';
 
-export default function Test() {
-  const navigation = useNavigation();
-  const [modalVisible, setModalVisible] = useState(false);
-
-  const handleButtonPress = () => {
-    setModalVisible(true);
-    setTimeout(() => {
-      navigation.navigate("UserSignIn");
-      setModalVisible(false);
-    }, 3000);
-  };
-
+const Test = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "pink",
-      }}
-    >
-      <Text>test</Text>
-
-      <Button title="Open Modal" onPress={handleButtonPress} />
-
-      <Modal
-        visible={modalVisible}
-        transparent={true}
-        animationType="fade"
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <CustomPopup
-            source={require("../assets/changePasswordLogo.png")}
-            text={"Submit Successfully"}
-            text2={
-              "Your document has been submitted successfully. It will take 24-48 hours for approval."
-            }
-          />
-        </View>
-      </Modal>
+    <View>
+      <CustomTabBar />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-});
+export default Test
+
+const styles = StyleSheet.create({})
