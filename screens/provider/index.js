fcafa4ef61b61c96012f7d8edd1332609859b1ff
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-12-09 11:49:55 +0500

    Update index.js

--- screens/provider/index.js
+++ screens/provider/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Home from "./Home";
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
 export default function ProviderRoutes() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: "none" },
+      }}
+    >
       <Tab.Screen name="ProviderHome" component={Home} />
     </Tab.Navigator>
   );
